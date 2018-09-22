92036394d721d0d650cc5cd4a064d4d4928c8d7a
Purcy Guo <purcyguo@gmail.com>
2018-09-22 22:46:10 +0800

    add js

--- main.js
+++ main.js
@@ -3,17 +3,19 @@ let popover = document.getElementById('popover');
 let wrapper = document.getElementById('wrapper');
 
 //第一种方法
-// poButton.addEventListener('click', function (e) {
+poButton.addEventListener('click', function (e) {
 
-//     function hide() {
-//         popover.style.display = 'none';
-//         this.removeEventListener("click", hide);
-//     }
-//     popover.style.display = 'block';
-//     document.addEventListener('click', hide);
-// });
+    function hide() {
+        popover.style.display = 'none';
+        this.removeEventListener("click", hide);
+    }
+    popover.style.display = 'block';
+    document.addEventListener('click', hide);
+});
 
-
+wrapper.addEventListener('click', function(e) {
+    e.stopPropagation();
+});
 
 //第二种方法
 // poButton.addEventListener('click', function(e){
@@ -27,9 +29,7 @@ let wrapper = document.getElementById('wrapper');
 
 // });
 
-// wrapper.addEventListener('click', function(e) {
-//     e.stopPropagation();
-// });
+
 
 // poButton.addEventListener('click', function(e){
 //     this.removeEventListener("click",hide);
