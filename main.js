4bb0429303464f155332c92513e39112414d01db
Purcy Guo <purcyguo@gmail.com>
2018-09-22 22:41:36 +0800

    popover-demo

---
+++ main.js
@@ -0,0 +1,46 @@
+let poButton = document.getElementById('showPopover');
+let popover = document.getElementById('popover');
+let wrapper = document.getElementById('wrapper');
+
+//第一种方法
+// poButton.addEventListener('click', function (e) {
+
+//     function hide() {
+//         popover.style.display = 'none';
+//         this.removeEventListener("click", hide);
+//     }
+//     popover.style.display = 'block';
+//     document.addEventListener('click', hide);
+// });
+
+
+
+//第二种方法
+// poButton.addEventListener('click', function(e){
+
+//     popover.style.display = 'block';  
+//     document.addEventListener('click', function (){
+//         popover.style.display = 'none';  
+//         //console.log(caller)
+//     },{once: true});
+
+
+// });
+
+// wrapper.addEventListener('click', function(e) {
+//     e.stopPropagation();
+// });
+
+// poButton.addEventListener('click', function(e){
+//     this.removeEventListener("click",hide);
+//     function hide() {
+//         popover.style.display = 'none';
+//         this.removeEventListener("click",hide);
+//     }
+//     popover.style.display = 'block'; 
+//     var timer = setTimeout(() => {
+//         document.addEventListener('click', hide);
+//         clearTimeout(timer)
+//     },200);
+
+// });
